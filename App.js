a8c8a49929db56c280f25026e8961c7266753417
Manuel Bahamonde <mbahamonde@primerosystems.com>
2024-02-21 18:00:38 -0300

    Minimal reproducible example

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
+import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [shouldRenderCamera, setShouldRenderCamera] = useState(false);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  useEffect(() => {
+    if (!permission?.granted) {
+      requestPermission();
+    }
+  }, [permission]);
+
+  const toggleCamera = () => setShouldRenderCamera((prevState) => !prevState);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {shouldRenderCamera && <Camera style={styles.camera} type={CameraType.back} />}
+      <Button title="Toggle Camera" onPress={toggleCamera} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  camera: {
+    height: '50%',
   },
 });
---
+++ README.md
@@ -0,0 +1,6 @@
+# Steps to reproduce
+1. Run the app with an iOS device in landscape orientation.
+2. Allow app to access Camera.
+3. Press "Toggle Camera" button, which will start rendering the camera. You'll notice the orientation is wrong.
+4. Rotate your device to portrait, and rotate it back to landscape. The camera's orientation will be fine now.
+5. You can press "Toggle Camera" button again to hide it and one more time to render it again, and you'll notice the orientation is wrong again.
